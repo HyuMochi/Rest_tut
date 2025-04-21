92708fea5bda577aff9abc448968a6b8d434f9c6
Hieu Phan <Hyumochi123@gmail.com>
2025-04-21 14:00:36 +0200

    Learned about middlewares

--- app.js
+++ app.js
@@ -2,9 +2,17 @@ const express = require('express');
 
 const app = express();
 
+//Middlewares, middlewares block you from going in to a page and logs a message in the terminal. 
+//app.use("/posts", () => {
+//    console.log("This is a middleware running, you are on the posts page")
+//})
 
-//routes, Home page
+
+//routes
+
+//Home page
 app.get('/', (req, res) => {
+    //send response
     res.send("on home");
 });
 
